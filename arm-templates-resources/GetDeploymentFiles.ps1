2f602143c225aa1ad67e90b77454cd2ef32e66d7
Angel Angelov <a.angelov@onebitsoftware.net>
2022-05-17 17:28:05 +0300

    updated parameters section

--- arm-templates-resources/GetDeploymentFiles.ps1
+++ arm-templates-resources/GetDeploymentFiles.ps1
@@ -1,2 +1,17 @@
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/OneBitSoftware/DevOps/azure/devops-agent/arm-templates-resources/InstallDevOpsAgent.ps1 -OutFile C:\Temp\InstallDevOpsAgent.ps1; C:\Temp\InstallDevOpsAgent.ps1
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/OneBitSoftware/DevOps/azure/devops-agent/arm-templates-resources/DeployQuantumServiceManager.ps1 -OutFile C:\Temp\DeployQuantumServiceManager.ps1; C:\Temp\DeployQuantumServiceManager.ps1
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory = $true)]$AzureDevOpsAccount,
+    [Parameter(Mandatory = $true)]$PersonalAccessToken,
+    [Parameter(Mandatory = $true)]$AgentName,
+    [Parameter(Mandatory = $true)]$PoolName,
+    [Parameter(Mandatory = $true)]$runAsAutoLogon,
+    [Parameter(Mandatory = $false)]$vmAdminUserName,
+    [Parameter(Mandatory = $false)]$vmAdminPassword
+)
+
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/OneBitSoftware/DevOps/azure/devops-agent/arm-templates-resources/InstallDevOpsAgent.ps1 -OutFile C:\Windows\Temp\InstallDevOpsAgent.ps1;
+& "C:\Windows\Temp\InstallDevOpsAgent.ps1" -AzureDevOpsAccount $AzureDevOpsAccount, -PersonalAccessToken $PersonalAccessToken, -AgentName $AgentName, -PoolName $PoolName, -runAsAutoLogon $runAsAutoLogon, -vmAdminUserName $vmAdminUserName, -vmAdminPassword $vmAdminPassword
+
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/OneBitSoftware/DevOps/azure/devops-agent/arm-templates-resources/DeployQuantumServiceManager.ps1 -OutFile C:\Windows\Temp\DeployQuantumServiceManager.ps1;
+& "C:\Windows\Temp\DeployQuantumServiceManager.ps1"
+

0719725a1d6f98fea1e3fd0fad8b784d036ad5a5
Teodor Ivanov <t.ivanov@onebitsoftware.net>
2024-10-09 13:50:55 +0300

    Newer version of powershell

--- scripts/Install-PowerShellCore.ps1
+++ scripts/Install-PowerShellCore.ps1
@@ -1,7 +1,7 @@
 ############################# Install PowerShell Core
 $ScriptsMainFolder = "C:\Install" # Full path with disk c:\install
-$PowerShellMsiFilename = "PowerShell-7.4.1-win-x64.msi"
-$PowerShellDownloadURL = "https://github.com/PowerShell/PowerShell/releases/download/v7.4.1/$PowerShellMsiFilename"
+$PowerShellMsiFilename = "PowerShell-7.4.5-win-x64.msi"
+$PowerShellDownloadURL = "https://github.com/PowerShell/PowerShell/releases/download/v7.4.5/$PowerShellMsiFilename"
 $PowerShellOutFile = "$ScriptsMainFolder\$PowerShellMsiFilename"
 
 Write-Host "Downloading $PowerShellDownloadURL"

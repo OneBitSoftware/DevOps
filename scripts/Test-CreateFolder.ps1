693e439b632b75fb18c2faf6519f90ac230a9641
Teodor Ivanov <t.ivanov@onebitsoftware.net>
2024-10-10 14:33:12 +0300

    Test function

--- scripts/Test-CreateFolder.ps1
+++ scripts/Test-CreateFolder.ps1
@@ -1,3 +1,5 @@
- # Create the install folder
-"1111p2222q3333".Split('pq')
-New-Item -ItemType Directory -Path "C:\Install\Test\T123"
+Function CreateFolder {
+    # Create the install folder
+    "1111p2222q3333".Split('pq')
+    New-Item -ItemType Directory -Path "C:\Install\Test\T123"
+}

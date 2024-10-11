eb01cec1665d630a62c4aa72d0b97b36ae7944f2
Teodor Ivanov <t.ivanov@onebitsoftware.net>
2024-10-11 08:53:21 +0300

    Tweaks

--- scripts/Setup-Main.ps1
+++ scripts/Setup-Main.ps1
@@ -1,4 +1,4 @@
-param ([Parameter(Mandatory)]$username, [Parameter(Mandatory)][SecureString] $password)
+param ([Parameter(Mandatory)]$username, [Parameter(Mandatory)]$password)
 
 try {
     $ScriptsMainFolder = "C:\Install" # Full path with disk c:\install
@@ -6,30 +6,17 @@ try {
     # Create the install folder
     New-Item -ItemType Directory -Path $ScriptsMainFolder
 
-
     ############################# Install PowerShell Core
     .\Install-PowerShellCore.ps1
     ############################# End PowerShell Core
 
-    #whoami
-
-    #& "C:\Program Files\PowerShell\7\pwsh.exe" "C:\Packages\Plugins\Microsoft.Compute.CustomScriptExtension\1.10.17\Downloads\0\Test-CreateFolder.ps1"
-
-    ############################# Execute custom PowerShell Core command
-    #Invoke-Command { & "pwsh.exe" } -NoNewScope
-    #$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
-    #Invoke-Command { & "pwsh.exe" } -NoNewScope
-    #pwsh Test-CreateFolder.ps1 
-    #Start-Process -FilePath "C:\Program Files\PowerShell\7\pwsh.exe" -ArgumentList "-ExecutionPolicy Bypass -Command `"C:\Packages\Plugins\Microsoft.Compute.CustomScriptExtension\1.10.17\Downloads\0\Test-CreateFolder.ps1`" -Wait"
-    # & "C:\Program Files\PowerShell\7\pwsh.exe" { "Hello!" }
-    # pwsh Test-CreateFolder.ps1 
-    ############################# End execute custom PowerShell Core command
-
+    ############################# Install PsExec
     Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
     C:\ProgramData\chocolatey\bin\choco.exe install -y sysinternals
+    ############################# Install PsExec
 
-    psexec -i -h -u $username -p $password pwsh.exe "C:\Packages\Plugins\Microsoft.Compute.CustomScriptExtension\1.10.17\Downloads\0\Test-CreateFolder.ps1"
+    psexec -i -h -u $username -p $password pwsh.exe "C:\Packages\Plugins\Microsoft.Compute.CustomScriptExtension\1.10.17\Downloads\0\Test-CreateFolder.ps1" *> test-createFolder.log
 } catch {
     $formatstring = "{0} : {1}`n{2}`n" +
                   "    + CategoryInfo          : {3}`n" +
